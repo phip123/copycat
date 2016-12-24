43b753cf2f20654c792730260478b930d03f15d7
Philipp <poepsi39@gmail.com>
2016-12-24 12:49:51 +0100

    Setup main view

---
+++ src/main/main/org/phip123/copycat/copy/process/configuration/Configuration.java
@@ -0,0 +1,26 @@
+package org.phip123.copycat.copy.process.configuration;
+
+/**
+ * Created by phip123 on 24.12.2016.
+ */
+public class Configuration {
+
+    private String source;
+    private String destination;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
