8453c365e7eb55b29f5d54e05d76f38ec42efecc
Philipp <poepsi39@gmail.com>
2016-12-24 12:51:36 +0100

    moved

--- src/main/java/org/phip123/copycat/Controller.java
+++
@@ -1,7 +0,0 @@
-package org.phip123.copycat;
-
-/**
- * Created by phip123 on 24.12.2016.
- */
-public class Controller {
-}
--- src/main/main/org/phip123/copycat/copy/result/Result.java
+++
@@ -1,7 +0,0 @@
-package org.phip123.copycat.copy.result;
-
-/**
- * Created by phip123 on 24.12.2016.
- */
-public interface Result {
-}
